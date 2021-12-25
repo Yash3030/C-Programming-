6b80afabc1237e7fa37303009f2ddeabde50f794
Yash3030 <89439505+Yash3030@users.noreply.github.com>
2021-12-25 16:01:30 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    int number 1,number 2, sum;
+    printf("enter first array");
+    scanf("%d",&number 1);
+    printf("enter second array");
+    scanf("%d",&number 2);
+    //calculating sum
+    sum=number+number2;
+    printf("\n addition of %d,%d,%d",number 1,number 2,sum);
+}
